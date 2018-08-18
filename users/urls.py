3faaf82ae8a7ffbf967db5a3320f12bc5e577653
Vinicius <viniciuscosta0197@gmail.com>
2018-08-18 10:03:22 -0300

    Using Django forms for login and registration forms

--- users/urls.py
+++ users/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("login", views.login, name="login"),
-    path("logout", views.logout, name="logout")
+    path("login", views.login_view, name="login_view"),
+    path("logout", views.logout_view, name="logout_view"),
+    path("signup", views.signup, name="signup")
 ]
--- users/views.py
+++ users/views.py
@@ -1,12 +1,15 @@
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth.models import User
 from django.shortcuts import render
 from django.urls import reverse
+
+from .forms import LoginForm, SignupForm
 
 def index(request):
     """ Renders main landing page """
     if not request.user.is_authenticated:
-        return render(request, "users/login.html", {"message": None})
+        return HttpResponseRedirect(reverse("login_view"))
     context = {
         "user": request.user
     }
@@ -14,16 +17,52 @@ def index(request):
 
 def login_view(request):
     """ Renders login view """
-    username = request.POST["username"]
-    password = request.POST["password"]
-    user = authenticate(request, username=username, password=password)
-    if user is not None:
-        login(request, user)
-        return HttpResponseRedirect(reverse("index"))
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            user = authenticate(request, email=email, password=password)
+            if user is not None:
+                login(request, user)
+                return HttpResponseRedirect(reverse("index"))
+            else:
+                return render(request, "users/login.html", {"message": "Invalid credentials."})
     else:
-        return render(request, "users/login.html", {"message": "Invalid credentials."})
+        form = LoginForm()
+
+    context = {
+        "message": None,
+        "form": form
+    }
+
+    return render(request, "users/login.html", context)
 
 def logout_view(request):
     """ Logout the user """
     logout(request)
     return render(request, "users/login.html", {"message": "Logged out."})
+
+def signup(request):
+    """ Register a new user """
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+
+        if form.is_valid():
+            email=  form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            user = User.objects.create_user(email, password)
+            user.save()
+
+            if user is not None:
+                login(request, user)
+                return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "users/signup.html")
+    else:
+        form = SignupForm()
+
+    context = {
+        "form": form
+    }
+    return render(request, "users/signup.html", context)
