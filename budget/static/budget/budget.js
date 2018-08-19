21b4745780f25f11fec572f7d7bdfd491f4b930c
Vinicius <viniciuscosta0197@gmail.com>
2018-08-19 13:51:03 -0300

    Add basic front-end category creation

--- budget/static/budget/budget.js
+++ budget/static/budget/budget.js
@@ -1,3 +1,43 @@
 document.addEventListener('DOMContentLoaded', ()=> {
-    alert("Hello!")
+    const DOM = {
+        category_groups: document.querySelector('.categorie_group'),
+        category: document.querySelector('.category')
+    }
+
+    function createCategoryGroup(category_name) {
+        const elem = document.createElement('div');
+        elem.appendChild(document.createTextNode(category_name));
+        elem.className = category_name;
+        return elem;
+    }
+
+    function addCategoryGroupToDOM(category_name) {
+        const elem = DOM.category_groups;
+        elem.appendChild(createCategoryGroup(category_name));
+    }
+
+    // Nests some category inside a category group
+    function createCategory(category) {
+        const elem = document.createElement('div');
+        elem.appendChild(document.createTextNode(category));
+        elem.className = 'category';
+        return elem;
+    }
+
+    // Creates a budgeting category
+    function addCategoryToDOM(category_group, category) {
+        const elem = document.getElementsByClassName(category_group);
+        elem[0].appendChild(createCategory(category));
+    }
+
+    document.getElementById('submit_category_group').onclick = () => {
+        const category_group = document.getElementById('category_group').value;
+        addCategoryGroupToDOM(category_group);
+    }
+
+    document.getElementById('submit_category').onclick = () => {
+        const category = document.getElementById('category').value;
+        const parent_group = document.getElementById('super').value;
+        addCategoryToDOM(parent_group, category)
+    }
 });
